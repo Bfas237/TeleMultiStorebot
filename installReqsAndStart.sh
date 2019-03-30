42d333aefa123f10585662963a0477bb09deb82d
Glitch (url-download) <none>
2019-03-30 18:32:00 +0000

    🔖🎪 Updated

--- installReqsAndStart.sh
+++ installReqsAndStart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-#pip3 install -U pip --user
-#pip3 install -U -r .requirements.txt --user 
-#python3 bot.py rs
+MD5=$(md5sum .requirements.txt | cut -f1 -d' ')
+if ! [ -d ".data/$MD5-site-packages" ]; then
+    rm -rf .data/*-site-packages
+    pip3 install -U -r .requirements.txt -t ".data/$MD5-site-packages" 
+fi
+exec env PYTHONPATH="$PWD/.data/$MD5-site-packages" python3 bot.py
