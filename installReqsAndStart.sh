3e226a8c1871dd15f5cfee4580eab27070870972
Glitch (harvest-hawthorn) <none>
2019-04-05 09:25:30 +0000

    🤒💉 Updated some ignored files

--- installReqsAndStart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MD5=$(md5sum .requirements.txt | cut -f1 -d' ')
-if ! [ -d ".data/$MD5-site-packages" ]; then
-    rm -rf .data/*-site-packages
-    pip3 install -U -r .requirements.txt -t ".data/$MD5-site-packages" 
-fi
-exec env PYTHONPATH="$PWD/.data/$MD5-site-packages" python3 bot.py
